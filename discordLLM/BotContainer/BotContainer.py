367208e667b6b2821fbf0b5af031b0650e7f74e7
Mikeymoo5 <moodids@outlook.com>
2024-06-17 22:23:00 -0500

    Added bot initialization and run command - Not tested

--- discordLLM/BotContainer/BotContainer.py
+++ discordLLM/BotContainer/BotContainer.py
@@ -1,6 +1,16 @@
 class BotContainer:
     def __init__(self, bot_token):
         self.token = bot_token
-        # self.client = discord.Client()
-        print("BotContainer initialized with token: " + self.token)
+
+        # Initialize the bot
+        self.intents = discord.Intents.default()
+        self.intents.message_content = True
+        self.client = discord.Client(intents = self.intents)
         
+
+    def run(self):
+        self.client.run(self.token)
+        
+    @self.client.event
+    async def on_ready():
+        print(f'{self.client.user} has connected to Discord!')
