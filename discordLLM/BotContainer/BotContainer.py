e0b45e6037f03998957d42f6064f451233af3bb4
Mikeymoo5 <moodids@outlook.com>
2024-06-18 21:42:06 -0500

    Added persona switching through a discord /command, as well as exception handling for if a persona does not exist

--- discordLLM/BotContainer/BotContainer.py
+++ discordLLM/BotContainer/BotContainer.py
@@ -1,33 +1,51 @@
 import discord
 from LLMHandler.LLMHandler import LLMHandler
+import toml
+import os
 class BotContainer:
-    def __init__(self, bot_token):
+    def __init__(self, bot_token, guild_id, llm_api, default_persona):
         self.token = bot_token
-
+        self.guild_id = guild_id
+        self.llm_api = llm_api
+        # Create the persona
+        self.create_persona(self.llm_api, default_persona)
         # Initialize the bot
         self.intents = discord.Intents.default()
         self.intents.message_content = True
-        self.client = discord.Client(intents=self.intents)
+        self.bot = discord.Bot(intents=self.intents)
         
     def run(self):
-        self.client.run(self.token)
+        self.bot.run(self.token)
 
-    def create_persona(self, api, model):
-        self.LLM = LLMHandler(api, model)
-        
+    def create_persona(self, api, persona_name):
+        config = toml.load("config.toml")  #TODO: CHANGE THIS TO USER PROVIDED PATH
+        try:
+            persona_name = persona_name.upper()
+            model = config[f"PERSONA-{persona_name}"]["model"]
+            prompt = config[f"PERSONA-{persona_name}"]["prompt"]
+        except KeyError:
+            print(f"Persona {persona_name} not found in config.toml")
+            return
+        self.LLM = LLMHandler(api, model, prompt)
+
     def register_events(self):
-        @self.client.event
+        @self.bot.event
         async def on_ready():
-            print(f'{self.client.user} has connected to Discord!')
+            print(f'{self.bot.user} has connected to Discord!')
         
-        @self.client.event
+        @self.bot.event
         async def on_message(message):
             # Prevent the bot from responding to itself
-            if message.author == self.client.user:
+            if message.author == self.bot.user:
                 return
             
             # If the bot is mentioned, respond with the LLM
-            if self.client.user in message.mentions: 
+            if self.bot.user in message.mentions: 
                 await message.reply(self.LLM.request(message.content))
+
+        @self.bot.slash_command(guild_ids=[self.guild_id])
+        async def changepersona(ctx, persona):
+            await ctx.respond(f"Switching persona to: {persona}")
+            self.create_persona(self.llm_api, persona)
 
             
--- discordLLM/__main__.py
+++ discordLLM/__main__.py
@@ -12,20 +12,17 @@ def main(config_path):
 
     # Load the configuration file
     config = toml.load("config.toml")
-    Bot = BotContainer(config["BOT"]["bot_token"]) # Create an instance of the BotContainer
-    Bot.create_persona(config["BOT"]["api"], "llama3") # Create an instance of the LLMHandler
+    Bot = BotContainer(config["BOT"]["bot_token"], config["BOT"]["guild_id"], config["BOT"]["api"], config["BOT"]["default_persona"]) # Create an instance of the BotContainer
+    # Persona creation is handled automagically by BotContainer
     Bot.register_events() # Register the events that the bot will respond to
     Bot.run() # Run the bot!
 
 def create_config():
     if click.prompt("No config file provided. Create one now? (y/n)").lower() == "y":
             bot_token = click.prompt("Enter your bot token")
-            while not bot_token:
-                bot_token = click.prompt("Enter your bot token")
+            guild_id = click.prompt("Enter your guild id")
             if click.prompt("Use the OpenAI api? (y/n)").lower() == "y":
                 openai_api_key = click.prompt("Enter your OpenAI api key")
-                while not openai_api_key:
-                    openai_api_key = click.prompt("Enter your OpenAI api key")
                 api = "OpenAI"
             else:
                 click.echo("Defaulting to Ollama")
@@ -38,6 +35,7 @@ def create_config():
     with open("config.toml", "w") as f:
         f.write("[BOT]\n")
         f.write(f"bot_token = \"{bot_token}\"\n")
+        f.write(f"guild_id = {guild_id}\n")
         f.write(f"api = \"{api}\"\n")
         if api == "OpenAI":
             f.write(f"openai_api_key = {openai_api_key}\n")
