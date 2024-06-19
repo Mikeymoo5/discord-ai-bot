6b52abb565709d8030dca67964aeb54815fe014e
Mikeymoo5 <moodids@outlook.com>
2024-06-19 12:32:22 -0500

    Finished support for changing config file

--- discordLLM/__main__.py
+++ discordLLM/__main__.py
@@ -2,23 +2,41 @@ import click
 import sys
 from BotContainer.BotContainer import BotContainer
 import toml
+import os
 @click.command()
 @click.option("--config-path", help="Path to the config file")
 def main(config_path):
-    if config_path:
-        click.echo("Using config file")
-    else:
-        create_config()
+    # Check for a config file
+    config_path = check_config(config_path)
 
     # Load the configuration file
-    config = toml.load("config.toml")
+    config = toml.load(config_path)
     Bot = BotContainer(config["BOT"]["bot_token"], config["BOT"]["guild_id"], config["BOT"]["api"], config["BOT"]["key"], config["BOT"]["default_persona"]) # Create an instance of the BotContainer
     # Persona creation is handled automagically by BotContainer
     Bot.register_events() # Register the events that the bot will respond to
     Bot.run() # Run the bot!
 
+def check_config(config_path):
+    if config_path:
+        if os.path.exists(config_path):
+            click.echo("The provided config file exists. Continuing...")
+            return config_path
+        else: 
+            click.echo("The provided config file does not exist.")
+            create_config()
+            return "config.toml"
+    elif os.path.exists("config.toml"):
+        click.echo("Found config.toml in working directory. Continuing...")
+        return "config.toml"
+    else:
+        click.echo("No config file provided.")
+        click.echo("No config file found in working directory.")
+        create_config()
+        return "config.toml"
+        
+
 def create_config():
-    if click.prompt("No config file provided. Create one now? (y/n)").lower() == "y":
+    if click.prompt("Create one in working directory? (y/n)").lower() == "y":
             bot_token = click.prompt("Enter your bot token")
             guild_id = click.prompt("Enter your guild id")
             if click.prompt("Use the OpenAI api? (y/n)").lower() == "y":
